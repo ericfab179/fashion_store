ed67b791f72c9cafb78f8eb75f0f191b0094df55
Angus Hunter <angus.hunter2001@gmail.com>
2021-09-02 16:43:44 +1200

    product table created with clothing inserted
    ref #4

--- setup/database_setup.sql
+++ setup/database_setup.sql
@@ -1,8 +1,17 @@
-CREATE TABLE papers (
-  code varchar(7),
-  name varchar(50) NOT NULL,
-  PRIMARY KEY (code)
+DROP TABLE IF EXISTS PRODUCTS;
+
+CREATE TABLE PRODUCTS(
+    product_id int(10) NOT NULL AUTO_INCREMENT,
+    product_category varchar(10) NOT NULL,
+    product_name varchar(10) NOT NULL,
+    product_price decimal(5, 2) NOT NULL,
+    product_desc varchar(50) NOT NULL,
+    product_quantity int(10) NOT NULL,
+    PRIMARY KEY(product_id)
 );
 
-INSERT INTO papers VALUES ('COSC326','Effective Programming');
-INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture');
+INSERT INTO products (product_id, product_category, product_name, product_price, product_desc, product_quantity) VALUES ('footwear', 'Birkenstocks', 120.00, 'Comfy and stylish', 100);
+INSERT INTO products (product_id, product_category, product_name, product_price, product_desc, product_quantity) VALUES ('tops', 'hoodie', 90.00, 'Oversized style in white', 100);
+INSERT INTO products (product_id, product_category, product_name, product_price, product_desc, product_quantity) VALUES ('top', 'crew neck', 85.00, 'Retro nike style', 100);
+INSERT INTO products (product_id, product_category, product_name, product_price, product_desc, product_quantity) VALUES ('bottoms', 'jeans', 130.00, 'Skinny jeans in black', 100);
+INSERT INTO products (product_id, product_category, product_name, product_price, product_desc, product_quantity) VALUES ('bottoms', 'sweatpants', 70.00, 'baggy grey sweatpants', 100);
