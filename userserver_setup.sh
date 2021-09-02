21175f4fe336956aa507ea569bb8811b71d33a0c
Angus Hunter <angus.hunter2001@gmail.com>
2021-09-02 13:07:08 +1200

    userserver setup implemented
    ref #2

---
+++ userserver_setup.sh
@@ -0,0 +1,13 @@
+echo "running userserver_setup.sh"
+echo "apt-get update"
+apt-get update
+echo "apt-get install -y apache2 php libapache2-mod-php php-mysql"
+apt-get install -y apache2 php libapache2-mod-php php-mysql
+echo "cp /vagrant/test-website.conf /etc/apache2/sites-available/"
+cp /vagrant/user-site.conf /etc/apache2/sites-available/
+echo "a2ensite test-website"
+a2ensite user-site
+echo "a2dissite 000-default"
+a2dissite 000-default
+echo "service apache2 reload"s
+service apache2 reload
