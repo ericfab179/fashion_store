061c7057dd58cdf1186ba3d7276d12413a4d4ed5
Angus Hunter <angus.hunter2001@gmail.com>
2021-09-05 13:28:04 +1200

    added cart functionality to all pages
    ref #11

--- www/user/bottoms.php
+++ www/user/bottoms.php
@@ -1,15 +1,18 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html lang="en">
     <head>
-        <title>HUNTER -> bottoms</title>
+        <title>HUNTER</title>
         <meta charset="utf-8">
         <link rel="icon" href="images/icon.png">
         <link rel="stylesheet" href="css/style.css"/>
+        <script src="js/jquery-3.6.0.min.js"></script>
+        <script src="js/cart.js"></script>
+        <script src="js/cartPopup.js"></script>
     </head>
     <body>
         <header>
             <a href="index.php">
-                <img src="images/logo.png" alt="HUNTER"> 
+                <img src="images/logo.png" id="hunter"> 
             </a>
             <nav>
                 <ul>
@@ -39,7 +42,25 @@
                     </table>
                 </div>
             </div>
-            <p>bottoms</p>
+            <div id="addToCartModal" class="modal">
+                <div class="modal-content">
+                    <span class="addToClose">&times;</span>
+                    <p>how many would you like?</p>
+                    <select id="quant">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                    </select>
+                    <button id="finalAdd">add to cart</button>
+                </div>
+            </div>
+            <p>all items</p>
             <table border="1" id="productTable" class="center">
                 <tr>
                     <th>product_id</th>
@@ -48,6 +69,7 @@
                     <th>product_price</th>
                     <th>product_desc</th>
                     <th>product_quantity</th>
+                    <th>add to cart</th>
                 </tr>
                 <?php
                     $db_host   = '192.168.2.13';
@@ -65,6 +87,7 @@
                         echo "<td>".$row["product_price"]."</td>";
                         echo "<td>".$row["product_desc"]."</td>";
                         echo "<td>".$row["product_quantity"]."</td>";
+                        echo "<td id='addToCart'><div><button>Add</button></div></td>";
                         echo "</tr>\n";
                     }
                 ?>
